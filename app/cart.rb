3dbad5eefa418b6e70db3cc7eb3d26403a71285f
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 00:04:04 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -1,3 +1,28 @@
 class Cart
   @@cart = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+
+      search_term = req.params["q"]
+
+      if @@cart.include?(search_term)
+        resp.write "#{search_term} is one of out items"
+      else 
+        resp.write "Couldn't find #{search_term}"
+      end
+
+    else
+      resp.write "Path Not Found"
+    end
+
+    resp.finish
+  end
 end
