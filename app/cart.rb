c8770a47e82510430bf1ce7f81076f60e879993d
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 00:24:55 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -8,21 +8,21 @@ class Cart
     if req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
-      end
+    end
     elsif req.path.match(/search/)
 
       search_term = req.params["q"]
 
-  elsif @@cart.empty?(search_term)
+    elsif @@cart.empty?(search_term)
       resp.write "Your cart is empty"
 
-      if @@cart.include?(search_term)
-        resp.write "#{search_term} is one of out items"
-      elsif @@cart.empty?(search_term)
-        resp.write "Your cart is empty"
-      else
-        resp.write "Path Not Found"
-      end
+    if @@cart.include?(search_term)
+      resp.write "#{search_term} is one of out items"
+    elsif @@cart.empty?(search_term)
+      resp.write "Your cart is empty"
+    else
+      resp.write "Path Not Found"
+    end
 
     resp.finish
   end
