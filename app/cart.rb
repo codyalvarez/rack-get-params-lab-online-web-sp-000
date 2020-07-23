f548be8f39c129f757dd3905b5c20879c14ff7bb
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 01:11:41 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -8,7 +8,7 @@ class Cart
   if req.path.match(/cart/) ## if were running this code, that means the user visited our site via at path /cart
     if @@cart.size == 0     ## if it is here that we check cart array to see if its empty
       resp.write "Your cart is empty"
-    elsif
+    elsif req.path.match(/cart/)
       @@cart.each do |item|
         resp.write "#{item}\n"
       else
