c1ab772ab9064f8acb01ff5f0886a014061691f3
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 01:36:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path.match(/cart/)
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    end
+
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
