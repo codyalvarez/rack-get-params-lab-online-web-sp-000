7d82c6346e91f2d3903b7de30131e668564c3573
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 01:14:49 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -5,8 +5,8 @@ class Cart
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-  if req.path.match(/cart/) 
-    if @@cart.size == 0     
+  if req.path.match(/cart/)
+    if @@cart.size == 0
       resp.write "Your cart is empty"
     elsif req.path.match(/cart/)
       @@cart.each do |item|
