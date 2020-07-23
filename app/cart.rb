29e2d3550a4ae29627ca5f28a2cee176fc745b73
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 00:23:30 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -5,13 +5,13 @@ class Cart
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    # if req.path.match(/cart/)
-    #   @@cart.each do |item|
-    #     resp.write "#{item}\n"
-    #   end
-    # elsif req.path.match(/search/)
-    #
-    #   search_term = req.params["q"]
+    if req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+
+      search_term = req.params["q"]
 
   elsif @@cart.empty?(search_term)
       resp.write "Your cart is empty"
