fe881d2a5a4807e83b2d75ceda129badbf07cd79
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 00:22:50 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -5,18 +5,23 @@ class Cart
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
+    # if req.path.match(/cart/)
+    #   @@cart.each do |item|
+    #     resp.write "#{item}\n"
+    #   end
+    # elsif req.path.match(/search/)
+    # 
+    #   search_term = req.params["q"]
 
-      search_term = req.params["q"]
+  elsif @@cart.empty?(search_term)
+      resp.write "Your cart is empty"
 
       if @@cart.include?(search_term)
         resp.write "#{search_term} is one of out items"
-      else
-        resp.write "Your cart is empty"
+      elsif @@cart.empty?(search_term)
+          resp.write "Your cart is empty"
+        else
+          resp.write "Path Not Found"
       end
 
     else
