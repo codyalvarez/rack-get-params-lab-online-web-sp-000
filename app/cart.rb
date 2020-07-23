02be7207e59aa161765c16715c042c42b9792700
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-23 01:07:54 +0000

    Automatically backed up by Learn

--- app/cart.rb
+++ app/cart.rb
@@ -6,7 +6,7 @@ class Cart
     req = Rack::Request.new(env)
 
   if req.path.match(/cart/) ## if were running this code, that means the user visited our site via at path /cart
-    if @@cart.size == 0     ## if it is here that we check cart array to see if its empty 
+    if @@cart.size == 0     ## if it is here that we check cart array to see if its empty
       resp.write "Your cart is empty"
     elsif
       @@cart.each do |item|
@@ -14,5 +14,5 @@ class Cart
       else
         "Path Not Found"
 
-    
+
     end
